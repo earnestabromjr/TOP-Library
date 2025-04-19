ac22b00b6c81826e43b8093c2d777c3b7b91bf35
earne <abromearnest@gmail.com>
2025-04-19 16:58:05 -0500

    Init project Structure
    Added functions to book.js
    Setup html skeleton and basic styles to get a working page.

--- book.js
+++ book.js
@@ -1,33 +1,51 @@
+const myLibrary = [];
+const libraryContainer = document.querySelector(".libraryContainer");
+
+
 // Book Constructor function
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw new Event("Book must be called with new");
   }
   this.read = read || false;
+  this.id = crypto.randomUUID();
 
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? "read" : "not read yet"
-    }`;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read yet"
+      }`;
   };
 }
+
+function addBookToLibrary(title, author, pages, read = false, library = myLibrary) {
+  const book = new Book(title, author, pages, read);
+  for (const existingBook of library) {
+    if (existingBook.id === book.id) {
+      throw new Error("Book with this ID already exists in the library.");
+    }
+  }
+  library.push(book);
+}
+
+// Display library contents
+function displayLibrary(library = myLibrary) {
+  libraryContainer.innerHTML = ""; // Clear previous contents
+  library.forEach(book => {
+    const bookElement = document.createElement("div");
+    bookElement.className = "book";
+    bookElement.textContent = book.info();
+    libraryContainer.appendChild(bookElement);
+  });
+}
+
 
 // Creating a new Book instance
 const book1 = new Book("1984", "George Orwell", 328, true);
 const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
-
-// Export the Book constructor and instances
-module.exports = {
-  Book,
-  book1,
-  book2,
-};
-
-console.log(book1.info()); // 1984 by George Orwell, 328 pages, read
-console.log(book2.info()); // To Kill a Mockingbird by Harper Lee, 281 pages, not read yet
+myLibrary.push(book1, book2);
+displayLibrary(myLibrary);
 
 console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype); //
 console.log(book1.valueOf());
--- howeWork.js
+++ howeWork.js
@@ -1,1 +1,20 @@
-new.target
+function getBrand(prefix) {
+  console.log(prefix + this.brand)
+}
+
+let honda = {
+  brand: 'Honda',
+};
+
+let audi = {
+  brand: 'Audi',
+};
+
+getBrand.call(honda, 'Its a "');
+getBrand.call(audi, 'Its a "');
+
+getBrand.apply(honda, ['Its a "']);
+getBrand.apply(audi, ['Its a "']);
+
+let getThis = () => this;
+console.log(getThis() === window);
